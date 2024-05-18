c308babd0aa8d0a7a20a0a684844d9d3103dd353
henry gad <henrygad.orji@gmail.com>
2024-05-18 21:47:59 +0100

    little changes on portfolio

--- portfolio/js/data.js
+++ portfolio/js/data.js
@@ -9,6 +9,13 @@ export const projectsData = [
         githubLink: 'https://github.com/henrygad/henryorji-portfolio'
     },
     {
+        title: 'Construction landing page',
+        description: 'The Box is a bulding construction based in the USA. They requires a modern, user-friendly website to showcase their products/services and engage with their target audience effectively. The website will serve as the primary online platform for him to establish their brand identity, attract potential customers, and drive conversions.',
+        imgUlr: './assest/construc.png',
+        liveLink: 'https://constructionlandingpagebyhenry.netlify.app/',
+        githubLink: 'https://github.com/henrygad/CODSOFT-level-1/tree/main/landing-page'
+    },
+    {
         title: 'traveling agent website',
         description: 'This website seeks to establish a robust online presence through a dynamic website that serves as a one-stop platform for travelers to explore destinations, plan trips, and book services. The website will embody the spirit of adventure while providing seamless functionality and inspiring visitors to embark on unforgettable journeys',
         imgUlr: './assest/travelling-webiste.png',
@@ -28,6 +35,13 @@ export const projectsData = [
         imgUlr: './assest/rock-paper-scissor-game.png',
         liveLink: 'https://rockpaperscissorgame-byhenry.netlify.app/',
         githubLink: 'https://github.com/henrygad/rock-paper-scissor-game'
+    },
+    {
+        title: 'A simple JS/CSS/HTL calculator',
+        description: "Built from scratch, this simple html, css, and js solve basic math calculations.",
+        imgUlr: './assest/cal.png',
+        liveLink: 'https://jscalculatorbyhenry.netlify.app/',
+        githubLink: 'https://github.com/henrygad/CODSOFT-level-1/tree/main/calculator'
     },
     {
         title: 'little lemon website',
